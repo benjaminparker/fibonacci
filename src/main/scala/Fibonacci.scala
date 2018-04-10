9a962ea3d5fec983fae54252148bedd47012761e
Benjamin Parker <benjamin@needstyping.com>
2018-04-10 07:46:19 +0100

    Removing ugly if else

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -1,7 +1,6 @@
 object Fibonacci {
-  def forPosition(i: Int) : Int = {
-    if (i == 0 || i == 1) i else {
-      forPosition(i - 2) + forPosition(i - 1)
-    }
+  def forPosition(i: Int) : Int = i match {
+    case 0 | 1 =>  i
+    case _ => forPosition(i - 2) + forPosition(i - 1)
   }
 }
